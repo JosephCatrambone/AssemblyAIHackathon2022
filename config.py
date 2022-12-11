5b470c3fa3ca075a34d7b3a15bdea8194b1ee199
Joseph Catrambone <jo.jcat@gmail.com>
2022-12-11 12:22:12 -0800

    Make environment paths optional for when we're hosting.  Last minute frantic update.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-STOCKFISH_EXECUTABLE_PATH = os.environ['STOCKFISH_PATH']  # i.e. "/home/joseph/Applications/stockfish/stockfish-9-64/stockfish-os-2022-x86-64-avx2.app"
-LICHESS_DATASET_PATH = os.environ['LICHESS_DATA_PATH']
+STOCKFISH_EXECUTABLE_PATH = os.environ.get('STOCKFISH_PATH')  # i.e. "/home/joseph/Applications/stockfish/stockfish-9-64/stockfish-os-2022-x86-64-avx2.app"
+LICHESS_DATASET_PATH = os.environ.get('LICHESS_DATA_PATH')
